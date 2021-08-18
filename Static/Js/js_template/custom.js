68fffa44a4bacd7aa9083a91b58062b7567a7bf9
1NE0 <juan.ortiz.alzate@correounivalle.edu.co>
2021-08-18 15:47:28 -0500

    custom images

--- Static/Js/js_template/custom.js
+++ Static/Js/js_template/custom.js
@@ -24,7 +24,7 @@
     var box = $(".header-text").height();
     var header = $("header").height();
 
-    if (scroll >= box - (header + 300)) {
+    if (scroll >= box - (header + 100)) {
       $("header").addClass("background-header");
     } else {
       $("header").removeClass("background-header");
