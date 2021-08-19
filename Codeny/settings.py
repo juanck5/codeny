cde0f5e3c5fa6737ee2559638f82c031add70ca7
1NE0 <juan.ortiz.alzate@correounivalle.edu.co>
2021-08-19 15:48:34 -0500

    despliegue

--- Codeny/settings.py
+++ Codeny/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'ryz2vtg4%t=mlpiwva@23@7!m%kyg0-(q^tgb39v7rcktrnj)_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
